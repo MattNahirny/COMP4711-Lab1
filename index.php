0d8a0efee3867ee8526045b80fde6c829f55d5c5
Matthew Nahirny <mnahirny1@my.bcit.ca>
2016-01-15 15:17:29 -0800

    Step 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 2016-01-15
+ * Time: 2:56 PM
+ */
+;
+$temp = 'Jim';
+echo 'Hi, my name is';
+echo $temp;
+$temp = 'geek';
+echo "I am a";
+echo $temp;
+$temp = 10;
+echo 'My level is';
+echo $temp;
