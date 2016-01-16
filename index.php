a257c31612bb5c5d70051075aa4a35e087824fcd
Matthew Nahirny <mnahirny1@my.bcit.ca>
2016-01-15 17:02:13 -0800

    Step 7

--- index.php
+++ index.php
@@ -30,11 +30,44 @@
 $position = $_GET['board'];
 $squares = str_split($position);
 
-if (winner('x',$squares)) echo 'You win.';
-else if (winner('o',$squares)) echo 'I win.';
-else echo 'No winner yet.';
-
-
+if (!isset($_GET['board'])) {
+    echo 'No board given';
+} else {
+    if (winner2('x', $squares)) echo 'You win.';
+    else if (winner2('o', $squares)) echo 'I win.';
+    else echo 'No winner yet.';
+}
+function winner2($token, $position)
+{
+    $result = false;
+    for ($row = 0; $row < 3; $row++) {
+        if ($position[3 * $row] == $token &&
+            $position[3 * $row + 1] == $token &&
+            $position[3 * $row + 2] == $token
+        )
+            $result = true;
+    }
+    for ($col = 0; $col < 3; $col++) {
+        if ($position[$col] == $token &&
+            $position[$col + 3] == $token &&
+            $position[$col + 6] == $token
+        )
+            $result = true;
+    }
+    if ($position[0] == $token &&
+        $position[4] == $token &&
+        $position[8] == $token
+    ) {
+        $result = true;
+    }
+    if ($position[2] == $token &&
+        $position[4] == $token &&
+        $position[6] == $token
+    ) {
+        $result = true;
+    }
+    return $result;
+}
 
 
 function winner($token, $position)
