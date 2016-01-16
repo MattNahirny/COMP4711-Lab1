8c84b245128afdb9cefe011820258cc3c67b2988
Matthew Nahirny <mnahirny1@my.bcit.ca>
2016-01-15 16:05:33 -0800

    Step 2

--- index.php
+++ index.php
@@ -5,13 +5,8 @@
  * Date: 2016-01-15
  * Time: 2:56 PM
  */
-;
-$temp = 'Jim';
-echo 'Hi, my name is';
-echo $temp;
-$temp = 'geek';
-echo "I am a";
-echo $temp;
-$temp = 10;
-echo 'My level is';
-echo $temp;
+$name = 'Jim';
+$what = 'geek';
+$level = 10;
+echo 'Hi, my name is '.$name,'. and I am a level '.$level.' 
+'.$what;
